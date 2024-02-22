c5d54a1c13a085ed8e8da6ec951ede129c0b64b3
Adaeze Ndupu <andupu4@gmail.com>
2024-02-22 13:38:33 +0100

    feat: add challenge mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Moon from "./assets/moon.svg";
 function App() {
   const [gameState, setGameState] = useState("start");
   const [theme, setTheme] = useState("light");
+  const [mode, setMode] = useState(null)
 
   useEffect(() => {
     if (theme === "light") {
@@ -20,17 +21,17 @@ function App() {
       return (
         <>
           <div className="absolute top-6 right-6 flex flex-col items-center gap-2 dark:text-white text-lg lg:text-2xl">
-            <div className="flex gap-4 p-2 rounded-full bg-white border border-grey-700 dark:border-transparent shadow w-28 justify-between">
+            <div className="flex gap-2 sm:gap-4 p-2 rounded-full bg-white border border-grey-700 dark:border-transparent shadow w-20 sm:w-28 justify-between">
               <img
                 src={Sun}
-                className={`size-10 p-2 ${
+                className={`size-7 sm:size-10 p-2 ${
                   theme === "light" && "bg-indigo-200 ring-2 ring-indigo-400"
                 } transition-all duration-300 ease-in-out cursor-pointer hover:bg-indigo-100 rounded-full`}
                 onClick={() => setTheme("light")}
               ></img>
               <img
                 src={Moon}
-                className={`size-10 p-2 ${
+                className={`size-7 sm:size-10 p-2 ${
                   theme === "dark" && "bg-indigo-200 ring-2 ring-indigo-400"
                 } transition-all duration-300 ease-in-out cursor-pointer hover:bg-indigo-100 rounded-full`}
                 onClick={() => setTheme("dark")}
@@ -38,31 +39,34 @@ function App() {
             </div>
           </div>
 
-          <StartScreen start={() => setGameState("play")} />
+          <StartScreen start={(mode) => {
+          setMode(mode);
+          setGameState("play");
+          }} />
         </>
       );
     case "play":
       return (
         <>
           <div className="absolute top-6 right-6 flex flex-col items-center gap-2 dark:text-white text-lg lg:text-2xl">
-            <div className="flex gap-4 p-2 rounded-full bg-white border border-grey-700 dark:border-transparent shadow w-28 justify-between">
+            <div className="flex gap-2 sm:gap-4 p-2 rounded-full bg-white border border-grey-700 dark:border-transparent shadow w-20 sm:w-28 justify-between">
               <img
                 src={Sun}
-                className={`size-10 p-2 ${
+                className={`size-7 sm:size-10 p-2 ${
                   theme === "light" && "bg-indigo-200 ring-2 ring-indigo-400"
                 } transition-all duration-300 ease-in-out cursor-pointer hover:bg-indigo-100 rounded-full`}
                 onClick={() => setTheme("light")}
               ></img>
               <img
                 src={Moon}
-                className={`size-10 p-2 ${
+                className={`size-7 sm:size-10 p-2 ${
                   theme === "dark" && "bg-indigo-200 ring-2 ring-indigo-400"
                 } transition-all duration-300 ease-in-out cursor-pointer hover:bg-indigo-100 rounded-full`}
                 onClick={() => setTheme("dark")}
               />
             </div>
           </div>
-          <PlayScreen end={() => setGameState("start")} />
+          <PlayScreen mode={mode} end={() => setGameState("start")} />
         </>
       );
     default:
--- src/Screens.jsx
+++ src/Screens.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import confetti from "canvas-confetti";
 import * as icons from "react-icons/gi";
 import { Tile } from "./Tile";
@@ -17,6 +17,12 @@ export const possibleTileContents = [
 ];
 
 export function StartScreen({ start }) {
+  const handleNormalPlay = () => {
+    start("normal");
+  };
+  const handleChallengePlay = () => {
+    start("challenge");
+  };
   return (
     <>
       <div className="grid min-h-screen place-content-center w-screen bg-white dark:bg-zinc-900">
@@ -29,10 +35,16 @@ export function StartScreen({ start }) {
             Flip over tiles looking for pairs
           </p>
           <button
-            onClick={start}
+            onClick={handleNormalPlay}
             className="text-white pt-2 pb-3 w-44 bg-gradient-to-t from-pink-600 to-pink-400 rounded-full text-3xl shadow-xl ring-2 ring-pink-400 transition-all duration-300 ease-out hover:scale-105"
           >
             Play
+          </button>
+          <button
+            onClick={handleChallengePlay}
+            className="text-white pt-2 pb-3 w-44 bg-gradient-to-t from-pink-600 to-pink-400 rounded-full text-3xl shadow-xl ring-2 ring-pink-400 transition-all duration-300 ease-out hover:scale-105 mt-6"
+          >
+            Challenge
           </button>
         </div>
       </div>
@@ -40,9 +52,50 @@ export function StartScreen({ start }) {
   );
 }
 
-export function PlayScreen({ end }) {
+export function PlayScreen({ mode, end }) {
   const [tiles, setTiles] = useState(null);
   const [tryCount, setTryCount] = useState(0);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [timeElapsed, setTimeElapsed] = useState(0);
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [scores, setScores] = useState({ player1: 0, player2: 0 });
+
+  const startTimer = () => {
+    setIsTimerRunning(true);
+  };
+
+  const stopTimer = () => {
+    setIsTimerRunning(false);
+  };
+
+  const resetGame = () => {
+    setTiles(null);
+    setTryCount(0);
+    setIsTimerRunning(false);
+    setTimeElapsed(0);
+    setCurrentPlayer(1);
+    setScores({ player1: 0, player2: 0 });
+  };
+
+  useEffect(() => {
+    let interval;
+    if (isTimerRunning) {
+      interval = setInterval(() => {
+        setTimeElapsed((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isTimerRunning]);
+
+  const formatTime = (timeInSec) => {
+    const minutes = Math.floor(timeInSec / 60);
+    const seconds = timeInSec % 60;
+
+    return `${minutes} : ${seconds < 10 ? "0" : ""}${seconds}`;
+  };
 
   const getTiles = (tileCount) => {
     // Throw error if count is not even.
@@ -76,12 +129,21 @@ export function PlayScreen({ end }) {
     const flippedTiles = tiles.filter((tile) => tile.state === "flipped");
     const flippedCount = flippedTiles.length;
 
+    if (flippedCount === 0) {
+      startTimer();
+    }
+
     // Don't allow more than 2 tiles to be flipped at once.
-    if (flippedCount === 2) return;
+    if (flippedCount === 2) {
+      return;
+    }
 
     // On the second flip, check if the tiles match.
     if (flippedCount === 1) {
       setTryCount((c) => c + 1);
+      setTimeout(() => {
+        setCurrentPlayer((prevPlayer) => (prevPlayer === 1 ? 2 : 1));
+      }, 1000);
 
       const alreadyFlippedTile = flippedTiles[0];
       const justFlippedTile = tiles[i];
@@ -93,6 +155,11 @@ export function PlayScreen({ end }) {
           ticks: 100,
         });
         newState = "matched";
+
+        setScores((prevScores) => ({
+          ...prevScores,
+          [`player${currentPlayer}`]: prevScores[`player${currentPlayer}`] + 1,
+        }));
       }
 
       // After a delay, either flip the tiles back or mark them as matched.
@@ -105,7 +172,11 @@ export function PlayScreen({ end }) {
 
           // If all tiles are matched, the game is over.
           if (newTiles.every((tile) => tile.state === "matched")) {
-            setTimeout(end, 0);
+            stopTimer();
+            // setTimeout(end, 0);
+            setTimeout(() => {
+              resetGame();
+            }, 3000);
           }
 
           return newTiles;
@@ -124,19 +195,84 @@ export function PlayScreen({ end }) {
   return (
     <>
       <div className="grid w-screen min-h-screen place-content-center bg-white dark:bg-gradient-to-r dark:from-zinc-800 dark:to-zinc-900">
-        <div className="flex gap-3 justify-center mb-12 items-center">
-          <div className="text-indigo-400 dark:text-white text-2xl lg:text-4xl font-medium">
-            Tries
-          </div>
-          <div className="px-3 lg:pb-1 bg-indigo-200 dark:bg-indigo-500 text-indigo-500 dark:text-white text-xl lg:text-3xl rounded-lg font-semibold flex items-center">
-            {tryCount}
-          </div>
-        </div>
+        <div className="lg:flex items-center gap-10">
+          <div>
+            <div className="text-center mb-4">
+              <p
+                className={`${
+                  mode === "challenge" ? "block" : "hidden"
+                } text-xl font-semibold text-indigo-500 dark:text-white mt-4`}
+              >
+                Current Player: Player{currentPlayer}
+              </p>
+            </div>
+            <div className="flex gap-3 justify-between mb-12 items-center">
+              <div className="flex gap-3 items-center">
+                <div className="text-indigo-400 dark:text-white text-2xl lg:text-3xl font-medium">
+                  Tries
+                </div>
+                <div className="px-3 lg:pb-1 bg-indigo-200 dark:bg-gradient-to-b dark:from-indigo-800 dark:to-indigo-900 text-indigo-500 dark:text-white text-xl lg:text-3xl rounded-lg font-semibold flex items-center">
+                  {tryCount}
+                </div>
+              </div>
 
-        <div className="grid grid-cols-4 mx-auto gap-5 p-5 bg-indigo-50 rounded-lg dark:bg-gradient-to-b dark:from-indigo-800 dark:to-indigo-900">
-          {getTiles(16).map((tile, i) => (
-            <Tile key={i} flip={() => flip(i)} {...tile} />
-          ))}
+              <div className="flex gap-3 items-center">
+                <div className="text-indigo-400 dark:text-white text-2xl lg:text-3xl font-medium">
+                  Time
+                </div>
+                <div className="px-3 lg:pb-1 bg-indigo-200 dark:bg-gradient-to-b dark:from-indigo-800 dark:to-indigo-900 text-indigo-500 dark:text-white text-xl lg:text-3xl rounded-lg font-semibold flex items-center">
+                  {formatTime(timeElapsed)}
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-4 mx-auto gap-5 p-5 bg-indigo-50 rounded-lg dark:bg-gradient-to-b dark:from-indigo-800 dark:to-indigo-900">
+              {getTiles(16).map((tile, i) => (
+                <Tile key={i} flip={() => flip(i)} {...tile} />
+              ))}
+            </div>
+            <div className="flex justify-center mt-6 gap-4">
+              <button
+                className="bg-indigo-300 hover:bg-indigo-400 hover:scale-105 dark:bg-gradient-to-b dark:from-indigo-800 dark:to-indigo-900 transition-all ease-in-out duration-300 text-xl lg:text-2xl rounded-md text-white py-2 px-4"
+                onClick={resetGame}
+              >
+                Reset Game
+              </button>
+              <button
+                className="bg-indigo-300 hover:bg-indigo-400 hover:scale-105 dark:bg-gradient-to-b dark:from-indigo-800 dark:to-indigo-900 transition-all ease-in-out duration-300 text-xl lg:text-2xl rounded-md text-white py-2 px-4"
+                onClick={() => setTimeout(end, 0)}
+              >
+                Home
+              </button>
+            </div>
+          </div>
+          <div
+            className={`${
+              mode === "challenge" ? "block" : "hidden"
+            } text-center mt-4`}
+          >
+            <p className="text-lg sm:text-xl lg:text-2xl font-semibold text-indigo-500 dark:text-white">
+              Score Board
+            </p>
+            <div className="flex lg:flex-col gap-5 justify-between mt-6 items-center">
+              <div className="flex gap-3 items-center">
+                <div className="text-indigo-400 dark:text-white text-2xl lg:text-3xl font-medium">
+                  Player 1
+                </div>
+                <div className="px-3 lg:pb-1 bg-indigo-200 dark:bg-gradient-to-b dark:from-indigo-800 dark:to-indigo-900 text-indigo-500 dark:text-white text-xl lg:text-3xl rounded-lg font-semibold flex items-center">
+                  {scores.player1}
+                </div>
+              </div>
+
+              <div className="flex gap-3 items-center">
+                <div className="text-indigo-400 dark:text-white text-2xl lg:text-3xl font-medium">
+                  Player 2
+                </div>
+                <div className="px-3 lg:pb-1 bg-indigo-200 dark:bg-gradient-to-b dark:from-indigo-800 dark:to-indigo-900 text-indigo-500 dark:text-white text-xl lg:text-3xl rounded-lg font-semibold flex items-center">
+                  {scores.player2}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
